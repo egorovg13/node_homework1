62434fb9266216849f0cb478426d8a84af0321a1
Grisha Egorov <egorovg13@gmail.com>
2020-08-20 22:08:31 +0300

    homework2almostcompleted

--- index.js
+++ index.js
@@ -1,54 +1,34 @@
-const fs = require('fs');
-const path = require('path');
-const { ok } = require('assert');
+const http = require('http');
+const Events = require('events');
+const eventEmitter = new Events();
 
-//let sourcePath = process.argv[2];
-let sourcePath = './testfolder/'
-let delSelector = true;
-let destinationName = 'sorted';
+const server = http.createServer((req, res) => {
 
-let destinationPath = './' + destinationName + '/';
+    if (req.url !== '/favicon.ico'){
+
+    eventEmitter.emit('user_connected', 'user connected')
+    eventEmitter.on('timestamp_sent', (string) => {
+        let dateObj = new Date();
+        
+        res.end(`${dateObj.getUTCDate()}.${dateObj.getUTCMonth()+1}, ${dateObj.getUTCHours()}:${dateObj.getUTCMinutes()}:${dateObj.getUTCSeconds()}`)
+
+    })}
+
+    }).listen(7006);
 
 
-const readDir = (base) => {
-    let files = fs.readdirSync(base);
+eventEmitter.on('user_connected', (string) => {
 
-    files.forEach(item => {
-        let itemPath = path.join(base, item);
-        let itemState = fs.statSync(itemPath);
+    console.log(string);
 
-        if (itemState.isDirectory()) {
-            readDir(itemPath);
-        } else {
-            copyToFolder(item, itemPath, destinationPath)
-        }
-    })
-};
-
-const copyToFolder = (itemName, itemPath ,destination) => {
-    let letterDirPath = path.join(destination, itemName.slice(0,1));
-    let newPath = path.join(letterDirPath, itemName);
-    const callback = () => {console.log('done')};
-    // let rs = fs.createReadStream(itemPath)
-    // let ws = fs.createWriteStream(newPath)
-
-    createDirectory(letterDirPath);
-
-    // rs.pipe(ws, 
-    //     {end: false}
-    //     ).on('end', () => {
-    //         console.log('done copying a file');
-    //     });
-
-    fs.createReadStream(itemPath).pipe(fs.createWriteStream(newPath), {end: false}).on('end', data => {console.log('to delete: ' + itemPath)})
-
-}
-
-const createDirectory = (path) => {
-    if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
-    };
-}
-
-createDirectory(destinationPath);
-readDir(sourcePath);
+    let timer = setInterval( () =>  {
+        let dateObj = new Date();
+        let currentTime = `${dateObj.getUTCDate()}.${dateObj.getUTCMonth()+1}, ${dateObj.getUTCHours()}:${dateObj.getUTCMinutes()}:${dateObj.getUTCSeconds()}`
+        console.log(currentTime)
+    }, 1000);
+    
+    let timeStop = setTimeout( () => {
+        clearInterval(timer)
+        eventEmitter.emit('timestamp_sent', 'timestamp sent')
+    }, 3000);
+});
