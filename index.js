f864400238cf57265588026b340d8ee9d94e75e7
Grisha Egorov <egorovg13@gmail.com>
2020-08-21 13:28:17 +0300

    homework2complete

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const http = require('http');
 const Events = require('events');
 const eventEmitter = new Events();
+const INTERVAL = parseInt(process.env.INTERVAL) || 1000;
+const TIME = parseInt(process.env.TIME) || 3000;
 
 const server = http.createServer((req, res) => {
 
     if (req.url !== '/favicon.ico'){
 
-    eventEmitter.emit('user_connected', 'user connected')
+        const isInteger = Number.isInteger(INTERVAL)
+        if (isInteger === false) {
+            console.log('interval is not an integer')
+        }
+
+    eventEmitter.emit('user_connected', `user connected with Interval ${INTERVAL} and Time ${TIME}`)
     eventEmitter.on('timestamp_sent', (string) => {
         let dateObj = new Date();
         
@@ -14,7 +21,7 @@ const server = http.createServer((req, res) => {
 
     })}
 
-    }).listen(7006);
+    }).listen(3000);
 
 
 eventEmitter.on('user_connected', (string) => {
@@ -25,10 +32,10 @@ eventEmitter.on('user_connected', (string) => {
         let dateObj = new Date();
         let currentTime = `${dateObj.getUTCDate()}.${dateObj.getUTCMonth()+1}, ${dateObj.getUTCHours()}:${dateObj.getUTCMinutes()}:${dateObj.getUTCSeconds()}`
         console.log(currentTime)
-    }, 1000);
+    }, INTERVAL);
     
     let timeStop = setTimeout( () => {
         clearInterval(timer)
         eventEmitter.emit('timestamp_sent', 'timestamp sent')
-    }, 3000);
+    }, TIME);
 });
