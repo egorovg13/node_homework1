16d255a503ec984f614d8b05363dedb382456ddb
Grisha Egorov <egorovg13@gmail.com>
2020-08-21 17:05:46 +0300

    homework1 complete

--- index.js
+++ index.js
@@ -2,67 +2,90 @@ const fs = require('fs');
 const path = require('path');
 const { ok } = require('assert');
 
-//let sourcePath = process.argv[2];
-let sourcePath = './testfolder/'
-let delSelector = true;
-let destinationName = 'sorted';
+let sourcePath = process.argv[2] || './testfolder/';
+let destinationName = process.argv[3] || 'sorted';
+let delSelector = process.argv[4] || true;
 
 let destinationPath = './' + destinationName + '/';
 
-
 const readDir = (base) => {
-    let files = fs.readdirSync(base);
+    fs.readdir(base, (error, files) => {
+        if (error) { 
+            console.log(error); 
+          } 
+        else { 
+            files.forEach(item => {
+                let itemPath = path.join(base, item);
 
-    files.forEach(item => {
-        let itemPath = path.join(base, item);
-        let itemState = fs.statSync(itemPath);
+                fs.stat(itemPath, (error, stats) => {
+                    if (error) {
+                        console.log(error)
+                    } else {
+                        if (stats.isDirectory()) {
+                            readDir(itemPath);
+                        } else {
+                            createFolder(item, itemPath)
+                        }
+                    }
+                })
+            })
+            }
+    })   
+}
 
-        if (itemState.isDirectory()) {
-            readDir(itemPath);
-        } else {
-            copyToFolder(item, itemPath, destinationPath);
-        }
-
-        console.log('reade to delete now')
-    })
-};
-
-const copyToFolder = (itemName, itemPath ,destination) => {
-    let letterDirPath = path.join(destination, itemName.slice(0,1));
+const createFolder = (itemName, itemPath) => {
+    let letterDirPath = path.join(destinationPath, itemName.slice(0,1));
     let newPath = path.join(letterDirPath, itemName);
-    const callback = () => {console.log('done')};
-
-    createDirectory(letterDirPath);
-
-    let rs = fs.createReadStream(itemPath)
-    let ws = fs.createWriteStream(newPath)
-    rs.pipe(ws).on('finish', () => {
-        console.log(`done copying file ${itemName}`);
-        fs.unlink(itemPath, () => {
-            console.log(`File ${itemPath} was deleted`)
-        });
+    
+    if (!fs.existsSync(letterDirPath)) {
+    fs.mkdir(letterDirPath, {recursive: true}, (error) => {
+        if (error) {
+            console.log(error);
+            return
+        }
+        console.log(`folder ${letterDirPath} was created!`);
+        moveFile(itemPath, newPath);
     });
 
-
-    // ws.on('finish', () => {
-    //             console.log('done copying a file');
-    //         })
-
-    // rs.pipe(ws, 
-    //     {end: false}
-    //     ).on('end', () => {
-    //         console.log('done copying a file');
-    //     });
-
-    // fs.createReadStream(itemPath).pipe(fs.createWriteStream(newPath), {end: false}).on('end', data => {console.log('to delete: ' + itemPath)})
-
+    } else {
+        moveFile(itemPath, newPath);
+    }
 }
 
-const createDirectory = (path) => {
-    if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
-    };
+const moveFile = (itemPath, newPath) => {
+    console.log(`we will move ${itemPath} to ${newPath}`);
+
+    fs.createReadStream(itemPath).pipe(fs.createWriteStream(newPath)).on('finish', () => {
+        let parentDir = path.dirname(itemPath);
+
+        fs.unlink(itemPath, () => {
+            console.log(`File ${itemPath} was deleted`);
+            if (delSelector) {
+                fs.readdir(parentDir, (error, files) => {
+
+                    if (error) {
+                        console.log(error);
+                        return
+                    } else {
+                        if (!files.length) {
+                            fs.rmdir(parentDir, () => { 
+                                console.log(`folder ${parentDir} deleted`); 
+                                    fs.readdir(path.dirname(parentDir), (error, files) => {
+                                        if (error) {
+                                            console.log(error)
+                                        } else if (!files.length) {
+                                            fs.rmdir(path.dirname(parentDir), (err) => {
+                                                if (err) {
+                                                }
+                                            })
+                                        }
+                                    })
+                            }); 
+                        }
+                    }
+                })}
+         });
+    });
 }
 
-createDirectory(destinationPath);
 readDir(sourcePath);
